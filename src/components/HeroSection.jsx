b886a5f168847bcce77c0c0712f7a35a1cb04600
Cipto Suhari <cipto.suheri@gmail.com>
2022-04-06 20:17:02 +0700

    add button call to action (CTA)

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -13,9 +13,16 @@ function HeroSection() {
             <p className="h2 font-medium text-slate-500 text-lg mb-5">
               Web Developer
             </p>
-            <p>
-              Belajar web programming itu menyenangkan dan mudah bukan? Bukan!
+            <p className="font-medium text-slate-400 mb-10 leading-relaxed">
+              Belajar web programming itu menyenangkan dan mudah bukan?{" "}
+              <span className="text-dark font-semibold">Bukan!</span>
             </p>
+            <a
+              href="#hubungi"
+              className="text-base font-semibold text-white bg-primary py-3 px-8 rounded-full hover:shadow-lg hover:opacity-80 transition duration-300 ease-in-out"
+            >
+              Hubungi Saya
+            </a>
           </div>
         </div>
       </div>
