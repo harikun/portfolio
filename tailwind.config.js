76f8df60a1299edeba1d9ad678433e59b2b188eb
Cipto Suhari <cipto.suheri@gmail.com>
2022-04-06 20:48:57 +0700

    custom container

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,17 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    container: {
+      center: true,
+      padding: "16px",
+    },
     extend: {
       colors: {
         primary: "#14b8a6",
         dark: "#0f172a",
+      },
+      screen: {
+        "2xl": "1320px",
       },
     },
   },
