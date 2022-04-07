fe450f73af9313bbde7fb03d5f1889849b772514
Cipto Suhari <cipto.suheri@gmail.com>
2022-04-07 23:22:06 +0700

    atyle navbar container

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,5 +1,13 @@
 function NavBar() {
-  return <header></header>;
+  return (
+    <header className="bg-transparent absolute top-0 left-0 w-full flex items-center z-10">
+      <div className="container ">
+        <div className="flex justify-between items-center relative">
+          <div className="px-4">{/* link disini  */}</div>
+        </div>
+      </div>
+    </header>
+  );
 }
 
 export default NavBar;
