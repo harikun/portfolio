6eb8188d97475c26b7ad19fc197898ff0599769f
Cipto Suhari <cipto.suheri@gmail.com>
2022-04-08 21:02:29 +0700

    add link to home and button hamburger

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,7 +3,26 @@ function NavBar() {
     <header className="bg-transparent absolute top-0 left-0 w-full flex items-center z-10">
       <div className="container ">
         <div className="flex justify-between items-center relative">
-          <div className="px-4">{/* link disini  */}</div>
+          <div className="px-4">
+            <a
+              href="#home"
+              className="font-bold text-lg text-primary block py-6"
+            >
+              Cipto Suhari
+            </a>
+          </div>
+          <div className="flex items-center px-4">
+            <button
+              id="hamburger"
+              name="hamburger"
+              type="button"
+              className="block absolute right-4"
+            >
+              <span></span>
+              <span></span>
+              <span></span>
+            </button>
+          </div>
         </div>
       </div>
     </header>
