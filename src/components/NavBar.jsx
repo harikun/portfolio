3b96f42031dcebcfc68dae00d2c40436139ecfb2
Cipto Suhari <cipto.suheri@gmail.com>
2022-04-08 21:55:46 +0700

    add link navigation

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,7 +1,9 @@
 function NavBar() {
   const hambuger = () => {
     const nav = document.querySelector("#hamburger");
+    const navMenu = document.querySelector("#nav-menu");
     nav.classList.toggle("hamburger-active");
+    navMenu.classList.toggle("hidden");
   };
 
   window.onscroll = function () {
@@ -32,13 +34,70 @@ function NavBar() {
               id="hamburger"
               name="hamburger"
               type="button"
-              className="block absolute right-4"
+              className="block absolute right-4 lg:hidden"
               onClick={hambuger}
             >
               <span className="origin-top-left hamburger-line transition duration-300 ease-in-out"></span>
               <span className="hamburger-line transition duration-300 ease-in-out"></span>
               <span className="origin-bottom-left hamburger-line transition duration-300 ease-in-out"></span>
             </button>
+
+            <nav
+              id="nav-menu"
+              className="hidden right-4 absolute top-full py-5 bg-white shadow-lg rounded-lg max-w-[250px] w-full lg:block lg:static
+              lg:bg-transparent lg:max-w-full lg:shadow-none lg:rounded-none"
+            >
+              <ul className="block lg:flex">
+                <li className="group">
+                  <a
+                    href="#home"
+                    className="text-base text-dark py-2 mx-8 group-hover:text-primary flex"
+                  >
+                    Beranda
+                  </a>
+                </li>
+                <li className="group">
+                  <a
+                    href="#about"
+                    className="text-base text-dark py-2 mx-8 group-hover:text-primary flex"
+                  >
+                    tentang Saya
+                  </a>
+                </li>
+                <li className="group">
+                  <a
+                    href="#portfolio"
+                    className="text-base text-dark py-2 mx-8 group-hover:text-primary flex"
+                  >
+                    Portfolio
+                  </a>
+                </li>
+                <li className="group">
+                  <a
+                    href="#clients"
+                    className="text-base text-dark py-2 mx-8 group-hover:text-primary flex"
+                  >
+                    Clients
+                  </a>
+                </li>
+                <li className="group">
+                  <a
+                    href="#blog"
+                    className="text-base text-dark py-2 mx-8 group-hover:text-primary flex"
+                  >
+                    Blog
+                  </a>
+                </li>
+                <li className="group">
+                  <a
+                    href="#contact"
+                    className="text-base text-dark py-2 mx-8 group-hover:text-primary flex"
+                  >
+                    Contact
+                  </a>
+                </li>
+              </ul>
+            </nav>
           </div>
         </div>
       </div>
