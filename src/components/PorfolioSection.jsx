232ef92a8cce8b925b5cad8253ffe581b3fcc33e
Cipto Suhari <cipto.suheri@gmail.com>
2022-04-09 08:43:16 +0700

    adding second and third portfolio then change image

--- src/components/PorfolioSection.jsx
+++ src/components/PorfolioSection.jsx
@@ -17,8 +17,42 @@ function PortfolioSection() {
             </p>
           </div>
         </div>
-        <div className="w-full px-4 flex flex-wrap justify-center">
-          <div className="mb-12 p-4">
+        <div className="w-full px-4 flex flex-wrap justify-center xl:w-10/12 xl:mx-auto">
+          <div className="mb-12 p-4 md:w-1/2">
+            <div className="rounded-md shadow-md overflow-hidden">
+              <img
+                src={require("../images/portfolio-3.png")}
+                alt="Python Script for Scraping Indonesian Website's Data"
+                className="w-full"
+              />
+              <h3 className="font-semibold text-lg text-dark mt-5">
+                Python Script web Indonesia
+              </h3>
+              <p className="font-medium text-base text-secondary">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi
+                ut lorem elit. In eros lectus. consectetur adipiscing elit.
+                Morbi ut lorem elit
+              </p>
+            </div>
+          </div>
+          <div className="mb-12 p-4 md:w-1/2">
+            <div className="rounded-md shadow-md overflow-hidden">
+              <img
+                src={require("../images/portfolio-2.png")}
+                alt="Canvas Gambar untuk anak-anak"
+                className="w-full"
+              />
+              <h3 className="font-semibold text-lg text-dark mt-5">
+                Canvas Gambar untuk anak-anak
+              </h3>
+              <p className="font-medium text-base text-secondary">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi
+                ut lorem elit. In eros lectus. consectetur adipiscing elit.
+                Morbi ut lorem elit
+              </p>
+            </div>
+          </div>
+          <div className="mb-12 p-4 md:w-1/2">
             <div className="rounded-md shadow-md overflow-hidden">
               <img
                 src={require("../images/portfolio-1.png")}
