60a7d1994cb64f0f51a82a87219852a6f8705393
Cipto Suhari <cipto.suheri@gmail.com>
2022-04-10 22:00:05 +0700

    add text area

--- src/components/ContactSection.jsx
+++ src/components/ContactSection.jsx
@@ -1,6 +1,6 @@
 function ContactSection() {
   return (
-    <section id="contact" className="pt-36 pb-32">
+    <section id="contact" className="pt-32 pb-32">
       <div className="container">
         <div className="w-full px-4">
           <div className="max-w-full mx-auto text-center mb-16">
@@ -15,29 +15,60 @@ function ContactSection() {
           </div>
         </div>
         <form>
-          <div className="w-full px-4 mb-8">
-            <label htmlFor="name" className="text-base font-bold text-primary">
-              Nama
-            </label>
-            <input
-              type="text"
-              id="name"
-              className="w-full bg-slate-200 text-dark p-3 rounded-md focus:outline-none focus:ring-primary focus:ring-1
+          <div className="w-full lg:w-2/3 lg:mx-auto">
+            <div className="w-full px-4 mb-8">
+              <label
+                htmlFor="name"
+                className="text-base font-bold text-primary"
+              >
+                Nama
+              </label>
+              <input
+                type="text"
+                id="name"
+                className="w-full bg-slate-200 text-dark p-3 rounded-md focus:outline-none focus:ring-primary focus:ring-1
             focus:border-primary"
-              placeholder="Nama"
-            />
-          </div>
-          <div className="w-full px-4 mb-8">
-            <label htmlFor="email" className="text-base font-bold text-primary">
-              Email
-            </label>
-            <input
-              type="email"
-              id="email"
-              className="w-full bg-slate-200 text-dark p-3 rounded-md focus:outline-none focus:ring-primary focus:ring-1
+                placeholder="Nama"
+              />
+            </div>
+            <div className="w-full px-4 mb-8">
+              <label
+                htmlFor="email"
+                className="text-base font-bold text-primary"
+              >
+                Email
+              </label>
+              <input
+                type="email"
+                id="email"
+                className="w-full bg-slate-200 text-dark p-3 rounded-md focus:outline-none focus:ring-primary focus:ring-1
             focus:border-primary"
-              placeholder="Email"
-            />
+                placeholder="Email"
+              />
+            </div>
+            <div className="w-full px-4 mb-8">
+              <label
+                htmlFor="message"
+                className="text-base font-bold text-primary"
+              >
+                Pesan
+              </label>
+              <textarea
+                type="email"
+                id="email"
+                className="w-full bg-slate-200 text-dark p-3 rounded-md focus:outline-none focus:ring-primary focus:ring-1
+            focus:border-primary h-32"
+                placeholder="Email"
+              />
+            </div>
+            <div className="w-full px-4">
+              <button
+                className="text-base w-full font-semibold transition duration-500 ease-in-out
+            text-white bg-primary py-3 px-8 rounded-full hover:opacity-80 hover:shadow-lg"
+              >
+                Kirim
+              </button>
+            </div>
           </div>
         </form>
       </div>
